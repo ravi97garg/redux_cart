1b74bcb7663b31c1f9582364ea741c2042598287
ravi@ttn.com <booler@ttn.com>
2019-04-09 14:17:20 +0530

    updated cart reducer

--- src/reducers/cart.reducer.js
+++ src/reducers/cart.reducer.js
@@ -10,14 +10,16 @@ const reducer = (state = initialState, action) => {
         case "ADD_ITEM": {
             console.log("hello from cart", action.id, state.list);
             const index = state.list.findIndex(x => x.id === action.id);
-            // state.totalPrice += state.list[index].price;
+            const productToAdd = products.filter((item) => {
+                return item.id === action.id
+            })[0];
             if (index === -1){
                 const list = [...state.list];
-                list.push({id: action.id,
-                    name: products.filter((item) => {
-                        return item.id === action.id
-                    })[0].name,
-                    quantity: 1
+                list.push({
+                    id: action.id,
+                    name: productToAdd.name,
+                    quantity: 1,
+                    price: productToAdd.price
                 });
                 let totalPrice = products.filter((item) => {
                     return item.id === action.id
@@ -28,14 +30,14 @@ const reducer = (state = initialState, action) => {
                 const list = [...state.list];
                 const index = list.findIndex(x => x.id === action.id)
                 list[index] = {...list[index], quantity: list[index].quantity+1}
-                let totalPrice = products.filter((item) => {
-                    return item.id === action.id
-                })[0].price+state.totalPrice;
+                let totalPrice = productToAdd.price+state.totalPrice;
                 return {list: list, totalPrice: totalPrice}
             }
         }
         case "DECREASE_ITEM": {
             const index = state.list.findIndex(x => x.id === action.id);
+            console.log("run log",state.totalPrice, state.list[index].price, state.list, index, action.id);
+            const totalPrice = state.totalPrice - state.list[index].price;
             let list = [];
             if(state.list[index].quantity === 1){
                 list = [...state.list.slice(0,index), ...state.list.slice(index+1)]
@@ -44,7 +46,6 @@ const reducer = (state = initialState, action) => {
                 elem.quantity -= 1;
                 list = [...state.list.slice(0,index), elem, ...state.list.slice(index+1)]
             }
-            const totalPrice = state.totalPrice - state.list[index].price;
             return {list, totalPrice}
         }
         case "REMOVE_ITEM": {
